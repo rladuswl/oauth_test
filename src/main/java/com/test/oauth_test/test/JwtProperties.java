3e8f56f4d80a920786b6a7726a101ee8259927ce
myeondii <khkkgon0118@gmail.com>
2022-09-15 00:18:42 +0900

    Delete JwtProperties.java

--- src/main/java/com/test/oauth_test/test/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.test.oauth_test.test;
-
-public interface JwtProperties {
-    String SECRET = "plovo";
-    int EXPIRATION_TIME =  60000*10*3; // 30분
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-}
